2f75193225fc12454e6c2c3a494bbca913f2f591
Dmitry Soboliev <19leva89@gmail.com>
2024-09-18 15:46:18 +0300

    useAction abstraction

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+export const CreateBoard = z.object({
+	title: z
+		.string({
+			required_error: 'Title is required',
+			invalid_type_error: 'Title is required',
+		})
+		.min(3, {
+			message: 'Title is too short',
+		})
+		.max(20, {
+			message: 'Title is too long, maximum 20 letters',
+		}),
+})
