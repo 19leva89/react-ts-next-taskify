a45cc17c89e32cdfb7e31f49c0ee09e4d5263558
Dmitry Soboliev <19leva89@gmail.com>
2024-09-18 16:41:40 +0300

    Form Components

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { createBoard } from '@/actions/create-board'
-
-import { FormInput } from './form-input'
-import { FormButtonSubmit } from './form-button-submit'
 import { useAction } from '@/hooks/use-action'
+import { createBoard } from '@/actions/create-board'
+import { FormInput } from '@/components/form/form-input'
+
+import { FormButtonSubmit } from './form-button-submit'
 
 export const Form = () => {
 	const { execute, fieldErrors } = useAction(createBoard, {
@@ -25,7 +25,7 @@ export const Form = () => {
 	return (
 		<form action={onSubmit}>
 			<div className="flex flex-col space-y-2">
-				<FormInput errors={fieldErrors} />
+				<FormInput id="title" label="Board title" errors={fieldErrors} />
 			</div>
 
 			<FormButtonSubmit />
