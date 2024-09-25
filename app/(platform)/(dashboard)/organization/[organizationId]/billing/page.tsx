c693f7a436f9deeef6ceb6f528314f0e4b7797cb
Dmitry Soboliev <19leva89@gmail.com>
2024-09-25 10:07:13 +0300

    upd Stripe & Board Limits

--- app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
@@ -1,12 +1,10 @@
-import { Suspense } from 'react'
-
 import { Separator } from '@/components/ui/separator'
 import { checkSubscription } from '@/lib/subscription'
 
 import { Info } from '../_components/info'
-import { ActivityList } from './_components/activity-list'
+import { SubscriptionButton } from './_components/subscription-button'
 
-const ActivityPage = async () => {
+const BillingPage = async () => {
 	const isPro = await checkSubscription()
 
 	return (
@@ -15,11 +13,9 @@ const ActivityPage = async () => {
 
 			<Separator className="my-4" />
 
-			<Suspense fallback={<ActivityList.Skeleton />}>
-				<ActivityList />
-			</Suspense>
+			<SubscriptionButton isPro={isPro} />
 		</div>
 	)
 }
 
-export default ActivityPage
+export default BillingPage
