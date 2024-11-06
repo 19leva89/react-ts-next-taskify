0ed69ddbf0460d9bf2b2f597f838219ef632f868
Dmitry Soboliev <19leva89@gmail.com>
2024-11-06 16:10:53 +0200

    card schema change title max length to 25 letters

--- actions/update-list/schema.ts
+++ actions/update-list/schema.ts
@@ -11,7 +11,7 @@ export const UpdateList = z.object({
 		.min(3, {
 			message: 'Title is too short',
 		})
-		.max(20, {
-			message: 'Title is too long, maximum 20 letters',
+		.max(25, {
+			message: 'Title is too long, maximum 25 letters',
 		}),
 })
